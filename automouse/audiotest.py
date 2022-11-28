a5e0a5db3e3f9c28ce33edf438303c8a7cc8b78c
DESKTOP-TL4L9S9\LGCNS <nheo.an@gmail.com>
2022-11-28 18:01:42 +0900

    automouse 최초 커밋

--- automouse/audiotest.py
+++ automouse/audiotest.py
@@ -3,6 +3,11 @@ import numpy as np
 
 CHUNK = 2 ** 10
 RATE = 44100
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
 
 p = pyaudio.PyAudio()
 for index in range(p.get_device_count()):
@@ -13,12 +18,22 @@ for index in range(p.get_device_count()):
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
                 frames_per_buffer=CHUNK, input_device_index=1)
 
+frames = []
+
 while (True):
-    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
+    datasrc = stream.read(CHUNK)
+    data = np.fromstring(datasrc, dtype=np.int16)
+    frames.append(datasrc)
     if np.average(np.abs(data)) > 100 :
         print(int(np.average(np.abs(data))))
 
 stream.stop_stream()
 stream.close()
-
 p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
